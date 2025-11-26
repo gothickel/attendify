15fd4485cda919f041dc3437dd1047dcefbbc645
gothickel <86227152+gothickel@users.noreply.github.com>
2025-11-26 19:27:57 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -17,14 +17,21 @@ def testdb():
             database="db_ac0ccf_att",
             use_pure=True
         )
-
+        
         cursor = db.cursor()
         cursor.execute("SELECT NOW()")
         result = cursor.fetchone()
-
+        
         return f"Connected to MySQL! Server time: {result[0]}"
     except Exception as e:
         return f"Database connection error: {str(e)}"
-
+        
+    try:
+        import cv2
+        print("OpenCV is installed. Version:", cv2.__version__)
+    except ImportError:
+        print("OpenCV (cv2) is NOT installed.")
+        
 if __name__ == "__main__":
     app.run()
+
