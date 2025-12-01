f08cebe3a27f6cf39ba4f3978dabaf28a97540e0
gothickel <86227152+gothickel@users.noreply.github.com>
2025-12-01 10:20:35 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -32,7 +32,7 @@ def attendance():
 # ------- IP CAMERA STREAMING ---------
 
 def generate_frames():
-    url = "https://shaunte-prebronze-destiny.ngrok-free.dev"   # DroidCam or IP Cam
+    url = "https://shaunte-prebronze-destiny.ngrok-free.dev/video"   # DroidCam or IP Cam
 
     cap = cv2.VideoCapture(url)
 
@@ -60,4 +60,5 @@ def camera():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
