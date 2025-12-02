ac3bd5c73c0f96001ab3b285f752108d5863bfd2
gothickel <86227152+gothickel@users.noreply.github.com>
2025-12-02 23:27:09 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,7 +4,7 @@ from flask import Flask, Response
 app = Flask(__name__)
 
 # --- CHANGE THIS TO YOUR CAMERA ---
-RTSP_URL = "rtsp://admin:Camera123@192.168.100.53:554/stream1"
+RTSP_URL = "rtsp://admin:Camera123@195.184.246.150:554/stream1"
 
 def generate_frames():
     cap = cv2.VideoCapture(RTSP_URL)
@@ -44,3 +44,4 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
